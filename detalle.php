6251d505a889a47d0d6910876e389d4c31bbb17b
israel <israelb.aceves@gmail.com>
2011-07-14 16:03:29 -0500

    detalle

---
+++ detalle.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title></title>
+<META NAME="keywords" CONTENT="">
+<META NAME="description" CONTENT="">
+<META NAME="robots" CONTENT="all">
+<link rel="shortcut icon" type="image/ico" href="favicon.png" />
+
+<link rel="stylesheet" type="text/css" media="screen, projection" href="includes/css/screen.css" />
+<link rel="stylesheet" type="text/css" media="screen, projection" href="includes/css/detalle.css" />
+<script type="text/javascript" src="includes/js/jquery-1.5.1.min.js"></script>
+<script type="text/javascript" src="includes/js/jquery-ui-1.8.11.custom.min.js"></script>
+<script type="text/javascript" src="includes/js/jquery.scrollFollow.js"></script>
+<script type="text/javascript" src="includes/js/autoresize.jquery.min.js "></script>
+<script type="text/javascript" src="includes/js/scripts.js"></script>
+<script type="text/javascript" src="includes/js/forma.js"></script>
+</head>
+
+<body>
+	
+    <div id="wrapper" class="wrapper"><!-- wrapper -->
+    		
+            <!-- contendio -->
+			<div class="contenido">
+            	<div class="wrap_contenido">
+                	
+                     <!-- entrada -->
+                    <div class="entrada">
+                   		 <h1 class="AmerTypeMdBTMedium">Una travesía multigeneracional.</h1>
+                   		 <p>Mientras las personas se movilizan hacia lo sagrado, pareciera que millones de animales viajan para darle sentido a su existencia, y no sólo preservar la especie. En diciembre, mientras la Basílica de la Virgen Guadalupe es centro de peregrinación humana, el Golfo de Cortés en México acoge la llegada de miles de ballenas grises en su migración anual. Estos animales viven durante el verano en el Océano Ártico junto a las costas de Alaska donde abunda la comida, y cuando baja la temperatura migran hacia el sur a lo largo de toda la costa de Norteamérica hasta llegar a estas aguas propicias para su reproducción, recorriendo 9,640 km.
+Como las ballenas grises, muchas especies migran como forma de vida. La mariposa Monarca, usando el sol como compás, realiza una travesía anual en busca de la asclepia.</p>
+<p>Mientras las personas se movilizan hacia lo sagrado, pareciera que millones de animales viajan para darle sentido a su existencia, y no sólo preservar la especie. En diciembre, mientras la Basílica de la Virgen Guadalupe es centro de peregrinación humana, el Golfo de Cortés en México acoge la llegada de miles de ballenas grises en su migración anual. Estos animales viven durante el verano en el Océano Ártico junto a las costas de Alaska donde abunda la comida, y cuando baja la temperatura migran hacia el sur a lo largo de toda la costa de Norteamérica hasta llegar a estas aguas propicias para su reproducción, recorriendo 9,640 km.
+Como las ballenas grises, muchas especies migran como forma de vida. La mariposa Monarca, usando el sol como compás, realiza una travesía anual en busca de la asclepia.</p>
+               			<img src="img/detalle01.jpg" width="615" height="410" alt="" />
+                        <p>Como las ballenas grises, muchas especies migran como forma de vida. La mariposa Monarca, usando el sol como compás, realiza una travesía anual en busca de la asclepia.</p>
+                    	<p>Muchas especies migran como forma de vida. La mariposa Monarca, usando el sol como compás, realiza una travesía anual en busca de la asclepia.</p>
+                       
+                        <div class="cita">
+                        	<p>"Como las ballenas grises, muchas especies</p>
+                       		<p>migran como forma de vida."</p>
+                        </div>
+                      
+                       
+                        <div class="datos">
+                        	<div class="fecha">1 week ago</div>
+                            <div class="tags">TAGS: 
+                                <span class="AmerTypeMdBTMedium">
+                                	<a class="black_link" href="#">supervivencia</a> 
+                                    <a class="black_link" href="#">ruta</a> 
+                                    <a class="black_link" href="#">animales</a>
+                                </span>
+                            </div>
+                        </div>
+                       
+                        
+                    </div>
+                    <!-- end entrada -->
+                    
+                    <!-- paginador -->
+                    <div class="paginador_detalle AmerTypeMdBTMedium">
+                    	<div class="ant"><a class="black_link" href="#"> < anterior </a></div>
+                        <div class="sig"><a class="black_link" href="#">siguiente > </a></div>
+                    </div>
+                    <!-- end paginador -->
+                    
+                    
+                    <!-- escribenos -->
+                    <div class="escribenos">
+                    	
+                        <div class="comentario"><!-- comentario -->
+                        	<p>"Hola, qué bien que estén recorriendo el mundo. Espero pronto vuelvan a escribir acerca de este increíble lugar. Les deseo lo mejor. Una abrazo desde Guadalajara."</p>
+                            <div class="dequien">   
+                                <div class="por">Fernando Iñiguez</div>
+                                <div class="ciudad">México</div>
+                            </div>
+                        </div><!-- end comentario -->
+                        <div class="comentario"><!-- comentario -->
+                        	<p>"Hola, qué bien que estén recorriendo el mundo. Espero pronto vuelvan a escribir acerca de este increíble lugar. Les deseo lo mejor. Una abrazo desde Guadalajara."</p>
+                            <div class="dequien">   
+                                <div class="por">Fernando Iñiguez</div>
+                                <div class="ciudad">México</div>
+                            </div>
+                        </div><!-- end comentario -->
+                        <div class="comentario"><!-- comentario -->
+                        	<p>"Hola, qué bien que estén recorriendo el mundo. Espero pronto vuelvan a escribir acerca de este increíble lugar. Les deseo lo mejor. Una abrazo desde Guadalajara."</p>
+                            <div class="dequien">   
+                                <div class="por">Fernando Iñiguez</div>
+                                <div class="ciudad">México</div>
+                            </div>
+                        </div><!-- end comentario -->
+                        
+                        <!-- forma -->
+                        <div id="forma">
+                            <form method="post" action="">
+                                <fieldset>
+                                    <p>Escribenos</p>                             
+                                    <label for="nombre" class="nombre">Nombre</label>
+                                    <input type="text" name="nombre" id="nombre" />
+                                 
+                                    <label for="email" class="email">Correo:</label>
+                                    <input type="text" name="email" id="email"/>
+                                 
+                                    <label for="pass" class="pass">Pa&iacute;s:</label>
+                                    <input type="text" name="pass" id="pais"/>
+                                    
+                                    <label for="pass" class="pass">Mensaje:</label>
+                                   
+                                    <textarea id="text_comentario" name=""></textarea>
+                                 
+                                  
+                                    <div class="boton"><a class="black_link AmerTypeMdBTMedium" href="">enviar > </a></div>
+                                </fieldset>
+                            </form>
+                        </div>
+                        <!-- end forma -->
+                        
+                    </div>
+                    <!-- escribenos -->
+                    
+                    
+                    <!-- footer -->
+                    <div class="footer">
+                    	<div class="titulo AmerTypeMdBTMedium">TACUBAYA VIAJA</div>
+                        <ul class="AmerTypeMdBTMedium">
+                        	<li><a class="black_link" href="#">flickr</a>.</li>
+                            <li><a class="black_link" href="#">facebook</a>.</li>
+                            <li><a class="black_link" href="#">twitter</a>.</li>
+                            <li class="rss"><a class="black_link" href="#">rss</a></li>
+                        </ul>
+                    </div>
+                    <!-- end footer -->
+                    
+                </div>
+            </div>
+            <!-- end contendio -->
+            
+            <!-- lateral -->
+            <div class="lateral">            	
+                
+                <!-- menu -->
+                <div id="menu" class="menu AmerTypeMdBTMedium">
+                	<div class="header">
+                		<div class="nombre AmerTypeMdBTMedium">TACUBAYA VIAJA</div>
+            			<div class="descripcion">bitácora de un viaje de ida y vuelta.</div>
+               		</div>
+                    <!--<a class="selected_menu"  href="">blog</a>-->
+                    
+                    <div id="blog" class="menu_opciones">
+                        <a class="uno"  href="">blog</a>
+                        <a class="dos"  href="">el proyecto</a>
+                        <a class="tres" href="">la ruta</a>
+                        <a class="cuatro" href="">calendario</a>
+                        <a class="cinco" href="">patrociandores</a>
+                        <a class="seis" href="">nosotros</a>
+                        <a class="siete" href="">contacto</a>
+                        <a class="none_dot ocho"  href="">___________ buscar></a>
+                        <a class="nueve" href="">archivo</a>
+                        <a class="diez" href="">links</a>
+                    </div>
+                    
+                    <div class="ubicacion">
+                    	<div class="texto">ubicaci&oacute;n actual</div>
+                        <div class="ciudad">Bogot&aacute;, Colombia</div>
+                    </div>
+              </div>
+              <!-- end menu -->
+                
+            </div>
+            <!-- end lateral -->
+			
+           
+
+     </div><!-- end wrapper -->
+
+
+    
+</body>
+</html>
